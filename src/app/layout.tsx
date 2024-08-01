0327037209fedc4240dea11ac82674e0bc49663a
neff <105642322+devincreasio@users.noreply.github.com>
2024-08-01 16:27:24 +0300

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google'
+
+import type { Metadata } from 'next'
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+    title: 'Increasio - Test project',
+}
+
+interface RootLayoutProps {
+    children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
+}
